c4e0e072cc9736d140127d2ed38f9c49e5de4f63
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2012-04-09 22:46:16 +0300

    added delete keyword mechanism

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,4 +16,12 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     ui->listWidget->addItem(new QListWidgetItem(ui->lineEdit->text()));
+    ui->lineEdit->setText("");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QListWidgetItem *current_item = ui->listWidget->currentItem();
+    ui->listWidget->removeItemWidget(current_item);
+    delete current_item;
 }
