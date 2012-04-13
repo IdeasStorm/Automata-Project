c0ce4bb187818a8da1e0792a0334eb0c5307fabd
yaman <m.y.t-91@hotmail.com>
2012-04-13 17:41:40 +0300

    DFA in one Finit State and some fix

--- DFA.h
+++ DFA.h
@@ -20,13 +20,22 @@ public:
 
     // Get
     NodeDFA * getStartState();
+    NodeDFA * getFinit_WordsState();
+    NodeDFA * getSeparate_wordsState();
     QSet<NodeDFA*> getFinitStates();
     QSet<NodeDFA*> getAllStates();
     QSet<NodeDFA*> getNonFinitStates();
     QList<char> getAlphabetic();
+    QList<char> getSeparate_wordsAlphabetic();
+    void addToFinitState(NodeDFA* state);
+    void addToState(NodeDFA* state);
     //Set
     void setStartState(NodeDFA *state);
+    void setFinit_WordsState(NodeDFA *state);
+    void setSeparate_wordsState(NodeDFA *state);
+
     void setAlphabetic(QList<char> alphabetic);
+    void setSeparate_wordsAlphabetic(QList<char> alphabetic);
     // simulate DFA (Run DFA)
     QHash<QString,int>  SimulateDFA(QString input); //Simulate DFA and return True or false
 
@@ -34,8 +43,10 @@ public:
 
 private:
     NodeDFA *StartState ;
-    NodeDFA *Separate_words;
+    NodeDFA *Separate_wordsState;
+    NodeDFA *Finit_wordsState;
     QList<char> Alphabetic ;
+    QList<char> Separate_wordsAlphabetic ;
     QSet<NodeDFA*> FinitStates;
     QSet<NodeDFA*> AllStates;
 
